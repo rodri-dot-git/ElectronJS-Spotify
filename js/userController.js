24b08498a3f5ad6882b3e444b208c19ba8fdf10e
deLeonRodrigo <rodri230998@gmail.com>
2019-05-29 22:42:39 -0500

    basic init

---
+++ js/userController.js
@@ -0,0 +1,8 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId: '010fde68a6df41048c87cc0855a2f5ce',
+    clientSecret: '22f72271c1ef4536b281b06e87795299'
+});
+
