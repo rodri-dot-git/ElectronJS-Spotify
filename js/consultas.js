0a8ad8f69e0122bb931fc83baf830b2aa21ab950
deLeonRodrigo <rodri230998@gmail.com>
2019-06-03 22:27:33 -0500

    bootstrap + foto

--- js/consultas.js
+++ js/consultas.js
@@ -11,6 +11,7 @@ function nombre(){
     var obj
     ipcRenderer.on('nombreR', (event, arg) => {
         $("#datos").text(arg.display_name);
+        $("#profile").attr("src", arg.images[0].url);
     });
     ipcRenderer.send('nombre');
 }
