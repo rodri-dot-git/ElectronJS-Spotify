eda647425c0a1773498d9e6a8cab6d4597956585
deLeonRodrigo <rodri230998@gmail.com>
2019-06-04 02:22:59 -0500

    scrollbar and idk

--- js/consultas.js
+++ js/consultas.js
@@ -13,5 +13,31 @@ function nombre(){
         $("#datos").text(arg.display_name);
         $("#profile").attr("src", arg.images[0].url);
     });
+    ipcRenderer.on('playlists', (event, arg) => {
+        console.log(arg);
+        for (var i = 0; i < arg.length; i++) {
+            console.log(arg[i].images[0].url);
+        $("#playlists").append(
+            '<div class="col-12 col-md-6 col-lg-3">' +
+				`<img src='${arg[i].images[0].url}' class="img rounded style="width=300px">` +
+				'<br>' +
+				'<h5 style="text-align: center">' + arg[i].name + '</h5>' +
+			'</div>'
+        )
+        }
+    });
+    ipcRenderer.on('popular', (event, arg) => {
+        console.log(arg);
+        for (var i = 0; i < arg.length; i++) {
+            console.log(arg[i].images[0].url);
+            $("#popular").append(
+                '<div class="col-12 col-md-6 col-lg-3">' +
+                `<img src='${arg[i].images[0].url}' class="img rounded" style="width=300px">` +
+                '<br>' +
+                '<h5 style="text-align: center">' + arg[i].name + '</h5>' +
+                '</div>'
+            )
+        }
+    });
     ipcRenderer.send('nombre');
 }
